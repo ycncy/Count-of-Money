0c07db29dfe427aa6cb162d58c74414b5546c9ab
Meriem <ramdani.meriem2001@gmail.com>
2023-11-20 14:43:38 +0100

    add cards

--- client/src/authentication/Profile.tsx
+++ client/src/authentication/Profile.tsx
@@ -7,10 +7,18 @@ const Profile: React.FC = () => {
       <div className="max-w-7xl mx-auto mt-8 p-10 bg-white rounded shadow-md">
         <div>
           <div className="flex items-center">
-            <img src='../img/logo.png' alt="Profile Picture" className="w-16 h-16 rounded-full mr-4"/>
-            <div>
-              <h1 className="text-2xl font-bold">Username</h1>
-              <p className="text-gray-600">Abonnement de type X</p>
+            <img src='../img/logo.png' alt="Profile Picture" className="w-20 h-20 rounded-full mr-4"/>
+            <div className="w-full lg:w-4/12 px-4 lg:order-1">
+              <div className="flex justify-center py-4 lg:pt-4 pt-8">
+                <div className="mr-4 p-3 text-center">
+                  <span className="text-sm text-blueGray-400">Username</span>
+                  <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">John</span>
+                </div>
+                <div className="lg:mr-4 p-3 text-center">
+                  <span className="text-sm text-blueGray-400">Level</span>
+                  <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">Premium</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
