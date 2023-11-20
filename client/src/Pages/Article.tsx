93dc7d4d4b4f920d2066f06aad7bd24e003602dc
BilalEpit <bilal.kebayer@epitech.eu>
2023-11-20 15:03:49 +0100

    hotFix/dev

--- client/src/Pages/Article.tsx
+++ client/src/Pages/Article.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import NavBarConnectedUser from '../composant/Navbar/NavBarConectedUser';
+import NavBarConnectedUser from '../composant/Navbar/NavBarConnectedUser';
 import CardArticle from '../composant/CardArticle/CardArticle';
 import Footer from '../composant/Footer/Footer';
 
 interface RouteParams {
-    id: string;
+  id: string;
 }
 
 const Article: React.FC = () => {
